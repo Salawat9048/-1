b2356ec58a712ea16797148ed49a4c40aaa1a969
Salawat9048 <salawat9048@outlook.com>
2024-05-23 12:02:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+course_name = "python"
+d = course_name
+print(d)
+completed_tasks = 12
+a = completed_tasks
+print(a)
+tasks_completion_time = 1.5
+b = tasks_completion_time
+print(b)
+average_task_completion_time = 12 / 1.5
+c = average_task_completion_time
+print(c)
+f = "completed task"
+e = "task completion time"
+g = "average tasks completion time"
+i = "course name"
+print(i, d, f, a, e, b, g, c, )
